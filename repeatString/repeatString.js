f2d0f5e4a0d2c661eb3eaa898c7580731abf70d1
Eric <mcguirk.eric@gmail.com>
2020-05-28 14:19:32 -0400

    Completed helloWorld and repeatString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    let output = ""
+    if (n < 0) {
+        return "ERROR"
+    } else {
+        for (i = 0; i < n; i++) {
+            output += s
+        }
+    }
+    return output
 }
 
 module.exports = repeatString
